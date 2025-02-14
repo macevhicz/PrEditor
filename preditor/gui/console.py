be3f5026f51d815796b0f3ac0f04f0f2ff0e19f5
Mark Mancewicz <markm@blur.com>
2025-02-14 15:01:42 -0800

    Fake code lines in traceback for stdin, because py3 doesn't include them

--- preditor/gui/console.py
+++ preditor/gui/console.py
@@ -378,7 +378,34 @@ class ConsolePrEdit(QTextEdit):
         """returns the completer instance that is associated with this editor"""
         return self._completer
 
-    def executeString(self, commandText, filename='<ConsolePrEdit>', extraPrint=True):
+    def storeWorkboxLines(self):
+        self.workbox_lines_by_workbox_name = {}
+        if sys.version_info.major >= 3:
+            name_templ = "{}/{}"
+            workboxTab = self.window().uiWorkboxTAB
+            self.workbox_lines_by_workbox_name = {}
+            for group_idx in range(workboxTab.count()):
+                group = workboxTab.widget(group_idx)
+                group_name = workboxTab.tabText(group_idx)
+
+                for workbox_idx in range(group.count()):
+                    workbox = group.widget(workbox_idx)
+                    workbox_name = group.tabText(workbox_idx)
+                    workbox_name = name_templ.format(group_name, workbox_name)
+
+                    workbox_lines = []
+                    for idx in range(workbox.lines()):
+                        workbox_lines.append(workbox.text(idx))
+                    self.workbox_lines_by_workbox_name[workbox_name] = workbox_lines
+
+    def executeString(
+        self, commandText, consoleLine=None, filename='<ConsolePrEdit>', extraPrint=True
+    ):
+        # These vars helps with faking code lines in tracebacks for stdin input, which
+        # workboxes are, and py3 doesn't include in the traceback
+        self.storeWorkboxLines()
+        self.consoleLine = consoleLine or ""
+
         if self.clearExecutionTime is not None:
             self.clearExecutionTime()
         cursor = self.textCursor()
@@ -433,6 +460,10 @@ class ConsolePrEdit(QTextEdit):
 
     def executeCommand(self):
         """executes the current line of code"""
+
+        # Not using workbox, so clear this
+        self.consoleLine = ""
+
         # grab the command from the line
         block = self.textCursor().block().text()
         p = '{prompt}(.*)'.format(prompt=re.escape(self.prompt()))
@@ -455,7 +486,9 @@ class ConsolePrEdit(QTextEdit):
                 self._prevCommands = self._prevCommands[-1 * self._prevCommandsMax :]
 
                 # evaluate the command
-                cmdresult, wasEval = self.executeString(commandText)
+                cmdresult, wasEval = self.executeString(
+                    commandText, consoleLine=commandText
+                )
 
                 # print the resulting commands
                 if cmdresult is not None:
@@ -793,6 +826,14 @@ class ConsolePrEdit(QTextEdit):
 
         return ret
 
+    @staticmethod
+    def getIndentForCodeTracebackLine(msg):
+        indent = ""
+        match = re.match(r"^ *", msg)
+        if match:
+            indent = match.group() * 2
+        return indent
+
     def write(self, msg, error=False):
         """write the message to the logger"""
         # Convert the stream_manager's stream to the boolean value this function expects
@@ -847,6 +888,34 @@ class ConsolePrEdit(QTextEdit):
             filename = info.get("filename", "") if info else ""
             isConsolePrEdit = '<ConsolePrEdit>' in filename
 
+            # For workboxes, use this regex pattern, so we can extract workboxName and
+            # lineNum
+            pattern = r'File "<Workbox(?:Selection)?>:(?P<workboxName>.*)", '
+            pattern += r'line (?P<lineNum>\d{1,6}), in'
+
+            match = re.search(pattern, msg)
+            isWorkbox = bool(match)
+
+            # Starting in Python 3, tracebacks don't include the code executed for
+            # stdin, so workbox code won't appear. This attempts to include it.
+            if sys.version_info.major >= 3:
+                if isWorkbox:
+                    workboxName = match.groupdict().get("workboxName")
+                    lineNum = int(match.groupdict().get("lineNum")) - 1
+
+                    workboxLines = self.workbox_lines_by_workbox_name.get(
+                        workboxName, None
+                    )
+                    lineContents = workboxLines[lineNum].lstrip()
+
+                    indent = self.getIndentForCodeTracebackLine(msg)
+                    msg = "{}{}{}".format(msg, indent, lineContents)
+
+                elif isConsolePrEdit:
+                    consoleLine = self.consoleLine
+                    indent = self.getIndentForCodeTracebackLine(msg)
+                    msg = "{}{}{}\n".format(msg, indent, consoleLine)
+
             if self.addSepNewline:
                 if sepPreditorTrace:
                     msg += "\n"
@@ -861,7 +930,6 @@ class ConsolePrEdit(QTextEdit):
                 fileEnd = info.get("fileEnd")
                 lineNum = info.get("lineNum")
 
-                isWorkbox = '<WorkboxSelection>' in filename or '<Workbox>' in filename
                 if isWorkbox:
                     split = filename.split('/')
                     workboxName = split[-1]
