a0dcae922307a4bac7d83e8a1b057a9c70477a92
Mark Mancewicz <markm@blur.com>
2025-08-26 07:59:13 -0700

    Move Preferences to a uiWorkboxSTACK page

--- preditor/gui/workbox_text_edit.py
+++ preditor/gui/workbox_text_edit.py
@@ -113,7 +113,7 @@ class WorkboxTextEdit(WorkboxMixin, QTextEdit):
         if cursor.selection().isEmpty():
             text = cursor.block().text()
 
-            selectText = self.window().uiSelectTextACT.isChecked() or selectText
+            selectText = self.window().uiSelectTextCHK.isChecked() or selectText
             if selectText:
                 cursor.select(QTextCursor.SelectionType.LineUnderCursor)
                 self.setTextCursor(cursor)
